ae4fce54a145718454ec5add9b43f141bb11cd2d
Gijs Verdonschot <flexkoenders@koendersadvies.nl>
2017-04-25 20:24:00 +0000

    Redirect from analytics to back, cause page is under construction

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,6 +2,7 @@ class AnalyticsController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        
+        redirect_to :back
+        flash[:warning] = "Deze pagina is nog onder constructie."
     end
 end
